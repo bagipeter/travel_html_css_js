35b10156c3a09a4fddeb6303f76acc5180441bc5
Bagi Péter <bgipeter877@gmail.com>
2025-04-06 12:34:55 +0200

    header,about section done

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const menu_btn = document.getElementById("menu_btn");
-
 const nav_links = document.getElementById("nav_links");
-
-const menu_icon = menu_btn.querySelector("i");
+const menu_i_tag = document.querySelector("i");
 
 menu_btn.addEventListener("click", (e) => {
-    nav_links.classList.toggle("open")
+  nav_links.classList.toggle("open");
+
+  const is_open = nav_links.classList.contains("open");
+
+  menu_i_tag.setAttribute(
+    "class",
+    is_open ? "ri-close-line" : "ri-menu-3-line"
+  );
+});
+
+nav_links.addEventListener("click", (e) => {
+  nav_links.classList.remove("open");
+  menu_i_tag.setAttribute("class", "ri-menu-3-line");
 });
